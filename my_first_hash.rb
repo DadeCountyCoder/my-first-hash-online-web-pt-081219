41514b62167be88dbba9c8faa3e74923e077b216
Alfredo Diaz <mail4alfredo@gmail.com>
2019-08-25 15:07:16 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,5 @@
 def my_hash
+  fav_cars = {"Subaru" => "Wrx", "Honda" => "Civic"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
